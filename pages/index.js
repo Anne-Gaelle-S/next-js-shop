7d5b7bed636db28660a52698627a43313d15b078
Anne-Gaelle-S <ang.schall@gmail.com>
2023-05-23 22:42:22 +0200

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Next shop</title>
+      </Head>
+      <main>
+        <h1>Next shop</h1>
+      </main>
+    </>
+  )
+}

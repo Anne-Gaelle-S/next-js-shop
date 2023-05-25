311915889d286a47f691f3ef6dc2d6d40c404643
Anne-Gaelle-S <ang.schall@gmail.com>
2023-05-25 18:45:58 +0200

    chore: install react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// Global setup comes here :
+const queryClient = new QueryClient();
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default App

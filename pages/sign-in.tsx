fe740a57038deac4c3ad2d035ecd2a82c90ce0fe
Anne-Gaelle-S <ang.schall@gmail.com>
2023-05-25 17:06:09 +0200

    feat: add sign-in form

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,9 +1,47 @@
+import { FormEventHandler, useState } from 'react';
+import Button from '../components/Button';
+import Field from '../components/Field';
+import Input from '../components/Input';
 import Page from '../components/Page';
+import { fetchJson } from '../lib/api';
 
 const SignInPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    const response = await fetchJson('http://localhost:1337/auth/local', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ identifier: email, password }),
+    });
+    console.log('sign in:', response);
+  };
+
   return (
     <Page title="Sign In">
-
+      <form onSubmit={handleSubmit}>
+        <Field label="Email">
+          <Input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </Field>
+        <Field label="Password">
+          <Input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </Field>
+        <Button type="submit">
+          Sign In
+        </Button>
+      </form>
     </Page>
   );
 };

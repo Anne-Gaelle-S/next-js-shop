738c46c944f0fa211d0ec68be3b8f992a832bf13
Anne-Gaelle-S <ang.schall@gmail.com>
2023-05-25 11:54:03 +0200

    feat: optimize image showed

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Product } from "../lib/products";
+import Image from "next/image";
 
 interface ProductCardProps {
   product: Product;
@@ -9,7 +10,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
     <div className="border my-4 w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
-        <img src={product.pictureUrl} alt="" />
+        <Image src={product.pictureUrl} alt="" width={320} height={240} />
         <div className="p-2 flex justify-between items-baseline">
           <h2 className="text-lg font-bold">
             {product.title}
